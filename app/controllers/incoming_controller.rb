3942c92c0cbd37479b22b0fed9c117512d47079a
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-04 22:27:34 -0400

    incoming controller fixes

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,9 +1,9 @@
-class IncomingController < ApplicationController
+#class IncomingController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: [:create]
-  skip_before_action :authenticate_user!
+#  skip_before_action :verify_authenticity_token, only: [:create]
+#  skip_before_action :authenticate_user!
 
-  def create
+#  def create
     #@user = User.find_by_email(email: params[:sender])  
     #if @user.nil?
     #  @user = User.create(email: params[:sender], password: "Welcome1", password_confirmation: "Welcome1")
@@ -11,7 +11,24 @@ class IncomingController < ApplicationController
     #@topic = Topic.find_or_create_by(title: params[:subject])
     #@bookmark = Bookmark.find_or_initialize_by(url: params["body-plain"], user: @user)
     #@bookmark.save   
-    head 200
-  end
+ #   head 200
+ # end
 
+#end
+class IncomingController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
+
+  def create
+    @user = User.find_by_email(params[:sender])
+    @topic = Topic.find_or_create_by(title: params[:subject])
+    @bookmark = @topic.bookmarks.find_or_create_by(url: params["body-plain"])
+
+    if @user.nil?
+      @user = User.create!(email: params[:sender], password: "Welcome1", password_confirmation: "Welcome1")
+    end
+
+    @bookmark.save
+    head 200
+  end
 end
