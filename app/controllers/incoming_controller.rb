6ea430063f9afaa903ae213b95baf7f6f909d825
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-04 22:13:35 -0400

    incoming controller fixes

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,9 +1,13 @@
 class IncomingController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
 
   def create
-    @user = User.find_or_create_by(email: params[:sender])  
+    @user = User.find_by_email(email: params[:sender])  
+    if user.nil?
+      @user = User.create(email: params[:sender], password: "Welcome1" password_confirmation: "Welcome1")
+    end
     @topic = Topic.find_or_create_by(title: params[:subject])
     @bookmark = Bookmark.find_or_initialize_by(url: params["body-plain"], user: @user)
     @bookmark.save   
