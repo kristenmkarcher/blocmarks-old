51d0611adcac40d0ea2b2905dd6e3271f6261191
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-04 22:21:33 -0400

    incoming controller fixes

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,7 +6,7 @@ class IncomingController < ApplicationController
   def create
     @user = User.find_by_email(email: params[:sender])  
     if user.nil?
-      @user = User.create(email: params[:sender], password: "Welcome1" password_confirmation: "Welcome1")
+      @user = User.create(email: params[:sender], password: "Welcome1", password_confirmation: "Welcome1")
     end
     @topic = Topic.find_or_create_by(title: params[:subject])
     @bookmark = Bookmark.find_or_initialize_by(url: params["body-plain"], user: @user)
