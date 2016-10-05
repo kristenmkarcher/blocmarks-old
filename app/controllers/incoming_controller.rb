d57062e60ba0f7bde74e37101e51d388787d622b
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-04 22:48:45 -0400

    incoming controller fixes

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,7 +2,7 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-
+    
     @user = User.find_by_email(params["sender"])
     @topic = Topic.find_by_title(params["subject"])
 
